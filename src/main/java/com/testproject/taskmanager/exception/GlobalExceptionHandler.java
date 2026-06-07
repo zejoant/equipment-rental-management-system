7845507bad1a6b9ec25625f67e1532bb57efb631
zejoant <77977238+zejoant@users.noreply.github.com>
2026-06-07 10:56:23 +0200

    exceptions yay

---
+++ src/main/java/com/testproject/taskmanager/exception/GlobalExceptionHandler.java
@@ -0,0 +1,23 @@
+package com.testproject.taskmanager.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.Map;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<?> handleRuntime(RuntimeException ex) {
+
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(Map.of(
+                        "error", ex.getMessage(),
+                        "status", 400
+                ));
+    }
+}
